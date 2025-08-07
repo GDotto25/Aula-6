543b25de9b3239bff60b5ee83bfa2913de8383fd
GDotto25 <gdottobt@gmail.com>
2025-08-06 21:52:56 -0300

    Update ex3.py

--- aula6/ex3.py
+++ aula6/ex3.py
@@ -7,16 +7,33 @@ Sexo: 'f' ou 'm';
 Estado Civil: 's', 'c', 'v', 'd';"""
 
 nome = input('Digite o seu nome: ')
-# entrada nome
-#validação do nome
 while len(nome)<3:
+    print('Nome inválido! Regidite o nome')
     nome = input('Digite o seu nome: ')
-#entrada idade
-idade = int(input('Digite a idade [0 - 150]'))
-#validação da idade
+
+idade = int(input('Digite a idade: '))
 while idade < 0 or idade>150:
-    idade = int(input('Digite a idade [0 - 150]')) 
-    print(f'Sua idade é: {idade}')
+    print('Idade inválido! Redigite a idade.')
+    idade = int(input('Digite a idade: '))
+
+sexo = input('Digite o sexo [f/m] ')
+sexo.lower() #coloca o que o usuário digitou em minúsculo
+while sexo != 'f' and sexo != 'm':
+    print('Sexo inválido! Redigite o sexo')
+    sexo = input('Digite seu sexo [f/m]')
+
+salario = float(input('Digite o seu salário: '))
+while salario < 1:
+     print('Salário inválido, digite novamente.')
 
 
+estado_civil = input('Digite seu estado civil: ').lower()
+while estado_civil != 's' and estado_civil != 'c' and estado_civil != 'v' and estado_civil != 'd':
+    print('Estado civil inválido! tente novamente.')
+    estado_civil = input('Digite seu estado civil: ')
 
+print(f'Nome: {nome}')
+print(f'Idade: {idade}')
+print(f'Sexo: {sexo}')
+print(f'Salário: {salario}')
+print(f'Estado civil: {estado_civil}')
